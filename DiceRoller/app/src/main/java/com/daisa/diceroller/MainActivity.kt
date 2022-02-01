55c7b64863bc8790ab124cb0f81ab0132ad19ee9
dhrax <davidfdez006@gmail.com>
2022-02-01 20:22:30 +0100

    Added dice images and roll function

--- DiceRoller/app/src/main/java/com/daisa/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/daisa/diceroller/MainActivity.kt
@@ -2,10 +2,44 @@ package com.daisa.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.daisa.diceroller.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.btnRoll.setOnClickListener {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val number : Int = Random.nextInt(6) + 1
+
+        val diceImage = when(number){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> Log.e("Daisa", "number obtanied not valid")
+        }
+
+        binding.imgvRoll.setImageResource(diceImage)
     }
 }
